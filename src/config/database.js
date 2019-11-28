abcc6731b13d296f47962deadd17956c10260bc8
Kairo Lamarca Santos <lamarca.kairo@gmail.com>
2019-11-28 14:08:10 -0300

    Configuração do multer e criação de rota para upload de arquivo

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,21 @@
+// module.exports = {
+//   dialect: 'postgres',
+//   host: 'localhost',
+//   username: 'postgres',
+//   password: 'docker',
+//   database: 'gobarber',
+//   define: {
+//     timestamps: true,
+//     underscored: true,
+//     underscoredAll: true,
+//   },
+// };
+
 module.exports = {
-  dialect: 'postgres',
+  dialect: 'mysql',
   host: 'localhost',
-  username: 'postgres',
-  password: 'docker',
+  username: 'root',
+  password: '1234',
   database: 'gobarber',
   define: {
     timestamps: true,
