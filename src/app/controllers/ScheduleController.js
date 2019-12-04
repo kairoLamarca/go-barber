da59d560e834a44eff8a8e9040cd5a09502cbe58
kairoLamarca <lamarca.kairo@gmail.com>
2019-12-04 13:48:26 -0300

    inicio do controller de Schedule

---
+++ src/app/controllers/ScheduleController.js
@@ -0,0 +1,9 @@
+import Appointment from '../models/Appointments';
+
+class ScheduleController {
+  async index(req, res) {
+    return res.json();
+  }
+}
+
+export default new ScheduleController();
