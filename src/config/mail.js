bc4e7c0ddd7847cfd50c1382ed20e540d2bbee93
kairoLamarca <lamarca.kairo@gmail.com>
2019-12-11 09:45:16 -0300

    envio de email com nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'd9879bf1ee57b7',
+    pass: '69b626fa5386fd',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
